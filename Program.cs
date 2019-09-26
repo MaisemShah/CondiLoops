1d80f7829fc7b2f521e42fad3c153eeafc77c6d8
MaisemShah <54338906+MaisemShah@users.noreply.github.com>
2019-09-26 12:33:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondiLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] huh = { "huh", "nooo", "llll" };
+            Console.WriteLine(huh[1]); 
+
+            foreach (string item in huh)
+            {
+                Console.WriteLine(huh);
+            }
+
+            float[] octopus = { 52f,652f,332f};
+            Console.WriteLine(octopus[1]);
+
+            foreach (float i in octopus)
+                if (i == 4f)
+                        
+            {
+                var num = i * 10;
+                Console.WriteLine(5);
+            }
+
+            else
+            {
+                Console.WriteLine("do it");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
